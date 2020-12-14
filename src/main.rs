cfcd36ac885b5e9809b0aef9b0ae33295a25e098
Ismael <ismalfmp@gmail.com>
2020-12-14 03:10:55 -0500

    cors

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,11 @@ mod todo_logic;
 mod repository;
 mod database_engine_pool;
 mod schema;
-use actix_cors::Cors;
+use actix_web::middleware;
 use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer, http};
 use crate::database_engine_pool::{connect, MysqlPool};
+use actix_cors::Cors;
 
 #[macro_use]
 extern crate diesel;
@@ -23,17 +24,17 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=debug");
     env_logger::init();
     HttpServer::new(|| {
-        let cors = Cors::default()
-            .allowed_methods(vec!["GET", "POST","PUT","DELETE"])
-            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
-            .allowed_header(http::header::CONTENT_TYPE)
-            .max_age(3600);
-        App::new().wrap(Logger::default()).wrap(cors)
+
+        App::new().wrap(Logger::default())
+            .wrap(middleware::DefaultHeaders::new().header("Access-Control-Allow-Heathers", "*"))
+            .wrap(middleware::DefaultHeaders::new().header("Access-Control-Allow-Methods", "*"))
+            .wrap(middleware::DefaultHeaders::new().header("Access-Control-Allow-Origin", "*"))
             .data(connect())
             .service(web_services::create_task)
             .service(web_services::update_task)
             .service(web_services::delete_task)
             .service(web_services::get_all)
+            .service(web_services::ok_task)
         //.service(web_services::get)
     })
         .bind("127.0.0.1:8088")
--- src/web_services/mod.rs
+++ src/web_services/mod.rs
@@ -1,5 +1,5 @@
 use actix_web::middleware::errhandlers::ErrorHandlerResponse;
-use actix_web::{dev, error, http, web, Error, HttpResponse, Result, Responder, get, put, post, delete};
+use actix_web::{dev, error, http, web, Error, HttpResponse, Result, Responder, get, put, post, delete, options};
 use serde::{Serialize, Deserialize};
 use crate::models::task::{Task, NewTask};
 use actix_web::web::Json;
@@ -24,28 +24,34 @@ pub async fn get(params: serde_json::Value, pool: web::Data<MysqlPool>) -> impl 
 
     format!("Hello {}! id:{}", 0, 0)
 }*/
+#[options("/*")]
+pub async fn ok_task() -> HttpResponse {
+    HttpResponse::Ok().status(http::StatusCode::OK).set_header("Access-Control-Allow-Headers","*").body("Ok")
+}
 
 #[post("/createTodo")]
-pub async fn create_task(data: Json<NewTask>, pool: web::Data<MysqlPool>) -> impl Responder {
+pub async fn create_task(data: Json<NewTask>, pool: web::Data<MysqlPool>) -> HttpResponse {
     println!("{}", data.name);
     // Use DAO create
-    web::block(move || taskDAO::save_item(data.into_inner(), get_conn(&pool).as_deref().unwrap())).await;
-
-    format!("Hello {}! id:{}", 0, 0)
+    let x = web::block(move || taskDAO::save_item(data.into_inner(), get_conn(&pool).as_deref().unwrap())).await;
+    let jsonRes = serde_json::to_string(&x.unwrap());
+    HttpResponse::Ok().status(http::StatusCode::OK).json(jsonRes.unwrap())
 }
 
 #[put("/updateTodo")]
-pub async fn update_task(data: Json<Task>, pool: web::Data<MysqlPool>) -> impl Responder {
+pub async fn update_task(data: Json<Task>, pool: web::Data<MysqlPool>) -> Result<HttpResponse, Error> {
     println!("{}", data.name);
     // Use DAO update
-    web::block(move || taskDAO::update_item(data.into_inner(), get_conn(&pool).as_deref().unwrap())).await;
-    format!("Hello {}! id:{}", 0, 0)
+    let x = web::block(move || taskDAO::update_item(data.into_inner(), get_conn(&pool).as_deref().unwrap())).await;
+    let jsonRes = serde_json::to_string(&x.unwrap());
+    Ok(HttpResponse::Ok().json(jsonRes.unwrap()))
 }
 
 #[delete("/deleteTodo/{id}")]
-pub async fn delete_task(id: web::Path<i32>, pool: web::Data<MysqlPool>) -> impl Responder {
+pub async fn delete_task(id: web::Path<i32>, pool: web::Data<MysqlPool>) -> Result<HttpResponse, Error> {
     println!("{}", id);
     // Use DAO delete operation
-    web::block(move || taskDAO::delete_item(id.into_inner(), get_conn(&pool).as_deref().unwrap())).await;
-    format!("Hello {}! id:{}", 0, 0)
+    let x = web::block(move || taskDAO::delete_item(id.into_inner(), get_conn(&pool).as_deref().unwrap())).await;
+    let jsonRes = serde_json::to_string(&x.unwrap());
+    Ok(HttpResponse::Ok().json(jsonRes.unwrap()))
 }
